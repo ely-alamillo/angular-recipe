a107bf07cf7a4acd428936e38e28f9fcbc06db0a
Ely Alamillo <ely0013@gmail.com>
2018-04-11 09:48:11 -0500

    added layout for recipe-list

--- client/src/app/recipes/recipe-list/recipe-list.component.ts
+++ client/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "A test recipe",
+      "This is a test",
+      "https://img0.etsystatic.com/000/0/5174401/il_fullxfull.191928196.jpg"
+    )
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
