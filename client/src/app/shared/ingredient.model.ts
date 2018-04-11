12a784669d668bc2abded4e739f9648a55ee543f
Ely Alamillo <ely0013@gmail.com>
2018-04-11 09:49:38 -0500

    added layout for shopping list

---
+++ client/src/app/shared/ingredient.model.ts
@@ -0,0 +1,4 @@
+export class Ingredient {
+  // ts shortcut
+  constructor(public name: string, public amount: number) {}
+}
--- client/src/app/shopping-list/shopping-list.component.ts
+++ client/src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  selector: "app-shopping-list",
+  templateUrl: "./shopping-list.component.html",
+  styleUrls: ["./shopping-list.component.css"]
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient("Turmeric", 1),
+    new Ingredient("Potatoes", 5)
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
