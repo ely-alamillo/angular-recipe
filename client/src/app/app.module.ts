2edc96391156775b6f874b6a80ba48db8881f62b
Ely Alamillo <ely0013@gmail.com>
2018-04-16 11:06:04 -0500

    added service to provider

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -11,6 +11,10 @@ import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 
+// directive
+import { DropdownDirective } from "./shared/dropdown.directive";
+import { ShoppingListService } from "./shopping-list/shopping-list.service";
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,10 +24,11 @@ import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-ed
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    DropdownDirective
   ],
   imports: [BrowserModule, FormsModule],
-  providers: [],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
