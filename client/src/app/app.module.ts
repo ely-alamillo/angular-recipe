8f433ea5c34373cf1e7b73890a4ddd3746120113
Ely Alamillo <ely0013@gmail.com>
2018-04-18 16:28:10 -0500

    wired out app router

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-ed
 // directive
 import { DropdownDirective } from "./shared/dropdown.directive";
 import { ShoppingListService } from "./shopping-list/shopping-list.service";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -27,7 +28,7 @@ import { ShoppingListService } from "./shopping-list/shopping-list.service";
     ShoppingEditComponent,
     DropdownDirective
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
