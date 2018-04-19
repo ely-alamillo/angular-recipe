1bd600c4236b13b07a2d7efad030fbbb52e18cf1
Ely Alamillo <ely0013@gmail.com>
2018-04-19 16:20:50 -0500

    changed from subscription to subject

--- client/src/app/shopping-list/shopping-list.service.ts
+++ client/src/app/shopping-list/shopping-list.service.ts
@@ -1,10 +1,11 @@
 import { Ingredient } from "../shared/ingredient.model";
 import { EventEmitter } from "@angular/core";
+import { Subject } from "rxjs/Subject";
 
 export class ShoppingListService {
   // lets our ingrdients list know we changed, otherwise it
   // doesn't update
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
 
   ingredients: Ingredient[] = [
     new Ingredient("Turmeric", 1),
@@ -18,11 +19,11 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
